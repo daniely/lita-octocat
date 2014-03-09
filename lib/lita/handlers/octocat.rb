609da4564437d0214e99c7837c2a51852f945eca
Daniel <robotright@gmail.com>
2014-03-09 10:08:50 +0800

    implement

--- lib/lita/handlers/octocat.rb
+++ lib/lita/handlers/octocat.rb
@@ -1,6 +1,21 @@
 module Lita
   module Handlers
     class Octocat < Handler
+      URL = 'https://api.github.com/octocat'
+
+      route %r{^octocat me$}i, :octocat, command: true, help: {
+        'octocat me' => 'Fetches random ascii octocat'
+      }
+
+      def octocat(response)
+        http_resp = http.get(URL)
+
+        if http_resp.status == 200
+          response.reply http_resp.body
+        else
+          Lita.logger.warn("Github octocat api could not be reached.")
+        end
+      end
     end
 
     Lita.register_handler(Octocat)
--- spec/lita/handlers/octocat_spec.rb
+++ spec/lita/handlers/octocat_spec.rb
@@ -1,4 +1,50 @@
 require "spec_helper"
 
 describe Lita::Handlers::Octocat, lita_handler: true do
+  it { routes_command("octocat me").to(:octocat) }
+
+  describe '#octocat' do
+    let(:response) { double("Faraday::Response") }
+
+    before do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(response)
+    end
+
+    it "replies with octocat ascii" do
+      ascii = <<EOS
+               MMM.           .MMM
+               MMMMMMMMMMMMMMMMMMM
+               MMMMMMMMMMMMMMMMMMM      _____________________________________________
+              MMMMMMMMMMMMMMMMMMMMM    |                                             |
+             MMMMMMMMMMMMMMMMMMMMMMM   | Half measures are as bad as nothing at all. |
+            MMMMMMMMMMMMMMMMMMMMMMMM   |_   _________________________________________|
+            MMMM::- -:::::::- -::MMMM    |/
+             MM~:~   ~:::::~   ~:~MM
+        .. MMMMM::. .:::+:::. .::MMMMM ..
+              .MM::::: ._. :::::MM.
+                 MMMM;:::::;MMMM
+          -MM        MMMMMMM
+          ^  M+     MMMMMMMMM
+              MMMMMMM MM MM MM
+                   MM MM MM MM
+                   MM MM MM MM
+                .~~MM~MM~MM~MM~~.
+             ~~~~MM:~MM~~~MM~:MM~~~~
+            ~~~~~~==~==~~~==~==~~~~~~
+             ~~~~~~==~==~==~==~~~~~~
+                 :~==~==~==~==~~
+EOS
+      allow(response).to receive(:status).and_return(200)
+      allow(response).to receive(:body).and_return(ascii)
+      send_command("octocat me")
+      expect(replies.last).to eq(ascii)
+    end
+
+    it "logs a warning on failure" do
+      allow(response).to receive(:status).and_return(500)
+      expect(Lita.logger).to receive(:warn).with(/could not be reached./)
+      send_command("octocat me")
+      expect(replies).to be_empty
+    end
+  end
 end
