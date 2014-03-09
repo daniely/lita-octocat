876c8894a9d02dbf990edda7ea78d9a3259bdc44
Daniel <robotright@gmail.com>
2014-03-09 09:13:02 +0800

    initial commit

---
+++ lib/lita/handlers/octocat.rb
@@ -0,0 +1,8 @@
+module Lita
+  module Handlers
+    class Octocat < Handler
+    end
+
+    Lita.register_handler(Octocat)
+  end
+end
---
+++ spec/lita/handlers/octocat_spec.rb
@@ -0,0 +1,4 @@
+require "spec_helper"
+
+describe Lita::Handlers::Octocat, lita_handler: true do
+end
